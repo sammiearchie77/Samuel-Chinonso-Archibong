12d94393c1a4f4a7daf9495099d119ef0421317c
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-10-10 23:18:53 +0100

    Created components that consist of avatar-card, blog, certification, details, education, error-page, experience, external-project, footer, head-tag-editor, lazy-image, project, skill, theme-changer. Created a helper and data directory holding helper utilities and json data

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
